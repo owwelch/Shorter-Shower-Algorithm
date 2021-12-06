4875ee58050a583f9efc9877faf00f027950899f
Owen Welch <92005935+owwelch@users.noreply.github.com>
2021-12-06 12:06:00 -0500

    Created data extraction script

--- model.py
+++ model.py
@@ -1,47 +1,60 @@
 import pandas as pd
-import time
 import csv
 import os
+import matplotlib.pyplot as plt
 
-def find_vals(df):
-    data = df.iloc[:,0].to_list()
+temp_col=0
+label_col=1
+
+def find_vals(file, train=False): #returns a list of temperature profiles for each detected shower in df.
+    df = pd.read_excel(file, sheet_name="DATA", usecols='C,D', skiprows = 1)
+    data = df.iloc[:,temp_col].to_list()
     n = 0
     vals = []
-    while n < len(data)-8:
-        t = True
-        for i in range(n, n+3):
-            if data[i+1] - data[i] < 0.1:
-                t = False
-                n = i+1
-                break
-        if t:
-            start_index = n
-            n += 3
-            peak_index = -1
-            while peak_index == -1:
-                t = True
-                for j in range(n, n+3):
-                    if data[j] < data[j+1]:
-                        t = False
-                        n = j+1
-                        break
-                if t:
-                    end_index = n
-                    vals.append([t for t in data[start_index:end_index])
-    return(vals)
+    det_starts = []
+    gt_starts = []
+    gt_ends = []
+    start_index = -1
+    print("detected:")
+    while n <= len(data)-3:
+        if all(data[i+1]-data[i]>=0.1 for i in range(n,n+3)):
+            if start_index == -1:
+                start_index = n
+        if start_index != -1 and all(data[i+1]<data[i] for i in range(n,n+3)):
+            end_index = n
+            det_starts.append(start_index)
+            vals.append([t for t in data[start_index:end_index]])
+            start_index = -1
+        n+=1
+    if train:
+        print("groundtruth:")
+        durs=[]
+        labels = df.iloc[:,label_col].to_list()
+        n = 0
+        while n<len(labels):
+            if str(labels[n]).startswith('Start'):
+                gt_starts.append(n)
+                start = n
+            if str(labels[n]).startswith('End'):
+                end = n
+                gt_ends.append(n)
+                durs.append(end-start)
+            n+=1
+        print(f"gt: {len(durs)}, detected: {len(vals)}")
+        print(gt_starts)
+        plt.plot(range(len(data)),data)
+        plt.ylabel(u"T (\N{DEGREE SIGN}F)")
+        plt.xlabel("index")
+        plt.title(f"Groundtruthed Temperature Data: {file[8:16]}")
+        plt.scatter(gt_starts,[data[i] for i in gt_starts], label="Starts")
+        #plt.scatter(gt_ends,[data[i] for i in gt_ends], label="Ends")
+        plt.scatter(det_starts,[data[i] for i in det_starts], label="Detected Starts")
+        plt.legend()
+        plt.show()
+    return
             
 def main():
     os.chdir('data')
-    file = os.listdir()[0]
-    df = pd.read_excel(file, sheet_name="DATA", usecols='B')
-    vals = find_vals(df)
-    result = open(os.path.abspath(os.getcwd())+ "\\results.csv", "x")
-    csvwriter = csv.writer(result, lineterminator = '\n')
-    header = ["start time","start temp (F)","stop time","stop temp (F)","peak time","peak temp(F)","duration (s)", '', "start row", "stop row", "peak row"]
-    csvwriter.writerow(header)
-    for i in range(len(vals)):
-        csvwriter.writerow(vals[i])
-    print("Done!")
-    result.close()
-
+    find_vals(os.listdir()[2], True)
+    
 main()
