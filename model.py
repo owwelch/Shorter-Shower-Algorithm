675809ba77e6ad2a808edf8d866114f7a4c61fa5
owwelch <owelch@oberlin.edu>
2022-04-27 18:02:56 -0400

    rewrote model as a script

--- model.py
+++ model.py
@@ -4,12 +4,11 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import random
-from keras.optimizers import Adam
+import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Dense, BatchNormalization, Normalization
+from keras.layers import Dense, BatchNormalization
 import numpy as np
 import sys
-import csv
 
 os.chdir('data')
 
@@ -32,60 +31,56 @@ for folder in os.listdir():
 # concatenating all our data into one big dataframe:
 data = pd.concat(list(dfs.values()), ignore_index = True)
 
-# start window size: 19
-# end window size: 25
-start_pts = []#list where the first item is 1 for start and 0 for not start
-end_pts = []#same list but for ends
+# is this a start or end model?
+model_type = sys.argv[1]
+
+
+if model_type == 'Start':
+    window_size = 19
+elif model_type == 'End':
+    window_size = 25
+else:
+    raise ValueError("invalid model type. 2nd argument must be either 'Start' or 'End'.")
+
+#list where the first item is 1 for model_type and 0 for not model_type:
+pts = []
 
 for i, row in data.iterrows():
     if not pd.isnull(row['label']):
-        if row['label'].startswith('Start'):
-            start_pts.append([1] + data.iloc[i-9:i+10]['temp'].tolist())
-        if row['label'].startswith('End'):
-            end_pts.append([1] + data.iloc[i-12:i+13]['temp'].tolist())
-initial_length = len(start_pts)
-if initial_length != len(end_pts):
-    print("inconsistent labels!")
+        if row['label'].startswith(model_type):
+            pts.append([1] + data.iloc[i-window_size//2:i+window_size//2+1]['temp'].tolist())
+
+initial_length = len(pts)
 
 #taking a random sample of indices (without replacement) of size k
-random_start_indices = random.sample(range(len(data) - 19), k =  len(data) - 19)
-random_end_indices = random.sample(range(len(data) - 25), k = len(data) - 25)
+random_indices = random.sample(range(len(data) - window_size), k =  len(data) - window_size)
 
-for i in random_start_indices:
-    if not (any(pd.isnull(data.iloc[i:i+19//2]['temp'])) or
-            (not pd.isnull(data.iloc[i+19//2]['label']) and data.iloc[i+19//2]['label'].startswith('Start'))):
-        start_pts.append([0] + data.iloc[i:i+19]['temp'].tolist())
-        if len(start_pts) == initial_length * 2:
-            break
+i = 0
+for _ in range(initial_length):
+    # none of the temp data can be null, and the midpoint can't be a pointed labeled with the model type.
+    while any(pd.isnull(data.iloc[i:i+window_size]['temp'])) or (
+        not pd.isnull(data.iloc[i+window_size//2]['label']) and data.iloc[i+window_size//2]['label'].startswith(model_type)
+    ):
+        i += 1
+    pts.append([0] + data.iloc[i:i+19]['temp'].tolist())
+    i += 1
 
-for i in random_end_indices:
-    if not (any(pd.isnull(data.iloc[i:i+25//2]['temp'])) or
-            (not pd.isnull(data.iloc[i+25//2]['label']) and data.iloc[i+25//2]['label'].startswith('End'))):
-        end_pts.append([0] + data.iloc[i:i+25]['temp'].tolist())
-        if len(end_pts) == initial_length * 2:
-            break
 
 #tensors holding the label, followed by a list of 10 temperatures:
-start_data = np.asarray(start_pts)
-np.random.shuffle(start_data)
+data = np.asarray(pts)
+np.random.shuffle(data)
 
-end_data = np.asarray(end_pts)
-np.random.shuffle(end_data)
+data_length = data.shape[0]
 
-data_length = start_data.shape[0]
-
-start_train = start_data[:int(data_length * .7)]
-start_val = start_data[int(data_length * .7):int(data_length * .85)]
-start_test = start_data[int(data_length * .85):]
-
-end_train = end_data[:int(data_length * .7)]
-end_val = end_data[int(data_length * .7):int(data_length * .85)]
-end_test = end_data[int(data_length * .85):]
+train = data[:int(data_length * .7)]
+val = data[int(data_length * .7):int(data_length * .85)]
+test = data[int(data_length * .85):]
 
 
-n = sys.argv[0]
+# power of two determining neuron count
+n = int(sys.argv[2])
 
-start_model = Sequential(
+model = Sequential(
     [
         BatchNormalization(),
         Dense(2**n, activation = 'relu', input_shape=(19,)),
@@ -93,48 +88,18 @@ start_model = Sequential(
     ]
 )
 
-start_model.compile(
+model.compile(
     loss='mse',
     metrics=['mae'],
-    optimizer = Adam(learning_rate=1e-5)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-5)
 )
 
-history = start_model.fit(
-    start_train[:,1:],
-    start_train[:,0],
+history = model.fit(
+    train[:,1:],
+    train[:,0],
     epochs = int(1e6),
-    validation_data = (start_val[:,1:], start_val[:,0]),
+    validation_data = (val[:,1:], val[:,0]),
     verbose = 0
 )
 
-with open(f'2^{n}_neurons_starts.csv', 'w') as file:
-    writer = csv.writer(file)
-    file.writerow(history.history['val_mae'])
-
-end_model = Sequential(
-    [
-        BatchNormalization(),
-        Dense(2**n, activation = 'relu', input_shape=(25,)),
-        Dense(1, activation = 'sigmoid')
-    ]
-)
-
-end_model.compile(
-    loss='mse',
-    metrics=['mae'],
-    optimizer = Adam(learning_rate=1e-5)
-)
-
-history = end_model.fit(
-    end_train[:,1:],
-    end_train[:,0],
-    epochs = int(1e6),
-    validation_data = (end_val[:,1:], end_val[:,0]),
-    verbose = 0
-)
-
-with open(f'2^{n}_neurons_ends.csv', 'w') as file:
-    writer = csv.writer(file)
-    file.writerow(history.history['val_mae'])
-
-
+print(','.join(history.history['val_mae']))
