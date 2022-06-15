b3ddf180383ebc4b8a3096038b5861ad185cf0e8
Owen Welch <owelch@hpc.cluster>
2022-06-15 12:42:34 -0400

    update script

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ for n in 0 1 2 3 4 5 6 7; do
 
 #SBATCH --job-name="owelch_script"
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=8192
+#SBATCH --mem-per-cpu=0
 #SBATCH -o "/home/students/owelch/Shorter-Shower-Algorithm/results/2^"${n}"_neurons_"$s".csv"
 #SBATCH -e "/home/students/owelch/Shorter-Shower-Algorithm/results/2^"${n}"_neurons_"$s".err"
 
